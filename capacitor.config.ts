29166f8943a8726bd747c74fe073055d4fd7f578
CRMTalk <aitalk@crmtalk.com.br>
2026-04-09 11:17:29 -0300

    Add iOS platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.radarjovemdovinho.app',
+  appName: 'Radar Jovem do Vinho',
+  webDir: 'dist'
+};
+
+export default config;
